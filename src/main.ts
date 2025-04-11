19a9927f3fa9345fb1a493741e15f4c1c98ac190
ibnu fadhil <fadhilgelaws@gmail.com>
2025-04-11 21:10:07 +0700

    setup pwa, og:images, and any other adjustments to make better seo

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import { bootstrapApplication } from "@angular/platform-browser"
 import { provideRouter } from "@angular/router"
 import { AppComponent } from "./app/app.component"
 import { routes } from "./app/app.routes"
-import { provideAnimations } from "@angular/platform-browser/animations"
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker'
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), provideAnimations()],
+  providers: [provideRouter(routes), provideAnimations(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })],
 }).catch((err) => console.error(err))
